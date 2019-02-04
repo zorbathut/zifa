168e768da5ede6f4aa57b99d9fd7ef1e74d9afce
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2019-02-04 04:20:16 -0800

    Add rewards analysis.

--- src/Prompt.cs
+++ src/Prompt.cs
@@ -10,6 +10,7 @@ public static class Prompt
     private static Regex PointRegex = new Regex("^gpoint( (?<token>[^ ]+))+$", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
     private static Regex ValueRegex = new Regex("^vendornet (?<amount>[0-9]+)( (?<token>[^ ]+))+$", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
     private static Regex AnalyzeRegex = new Regex("^analyze( (?<token>[^ ]+))+$", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
+    private static Regex RewardsRegex = new Regex("^rewards( (?<token>[^ ]+))+$", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
 
     public static void Run()
     {
@@ -21,6 +22,7 @@ public static class Prompt
             Dbg.Inf("  gpoint wind coin");
             Dbg.Inf("  vendornet 2000 tomestone poetic");
             Dbg.Inf("  analyze craftsman vi");
+            Dbg.Inf("  rewards nickel turban high steel fending");
             Dbg.Inf("");
 
             string instr = Console.ReadLine();
@@ -37,6 +39,14 @@ public static class Prompt
             else if (AnalyzeRegex.Match(instr) is var amatch && amatch.Success)
             {
                 DoItemAnalysis(Db.ItemLoose(amatch.Groups["token"].Captures.OfType<System.Text.RegularExpressions.Capture>().Select(cap => cap.Value).ToArray()));
+            }
+            else if (RewardsRegex.Match(instr) is var rmatch && rmatch.Success)
+            {
+                DoRewardsAnalysis(rmatch.Groups["token"].Captures.OfType<System.Text.RegularExpressions.Capture>().Select(cap => cap.Value).ToArray());
+            }
+            else
+            {
+                Dbg.Inf("nope nope nope");
             }
         }
     }
@@ -174,6 +184,59 @@ public static class Prompt
             Dbg.Inf($"  Market profit adjustment (10): {Commerce.MarketProfitAdjuster(1, item.Key, 10)}");
             Dbg.Inf($"  Market profit adjustment (99): {Commerce.MarketProfitAdjuster(1, item.Key, 99)}");
             Dbg.Inf($"  Market profit adjustment (stack): {Commerce.MarketProfitAdjuster(1, item.Key, item.StackSize)}");
+        }
+    }
+
+    public static void DoRewardsAnalysis(string[] tokens)
+    {
+        var prospective = new List<SaintCoinach.Xiv.Quest>();
+        var rewards = new HashSet<SaintCoinach.Xiv.QuestRewardItem>();
+        foreach (var quest in Db.GetSheet<SaintCoinach.Xiv.Quest>())
+        {
+            bool valid = true;
+            foreach (var token in tokens)
+            {
+                bool found = false;
+                foreach (var group in quest.Rewards.Items)
+                {
+                    foreach (var item in group.Items)
+                    {
+                        if (item.Item.Name.ToString().IndexOf(token, StringComparison.CurrentCultureIgnoreCase) != -1)
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!found)
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+            {
+                prospective.Add(quest);
+
+                foreach (var group in quest.Rewards.Items)
+                {
+                    foreach (var reward in group.Items)
+                    {
+                        if (reward.Item.Key != 0)
+                        {
+                            rewards.Add(reward);
+                        }
+                    }
+                }
+            }
+        }
+
+        Dbg.Inf($"{prospective.Count} matches");
+        foreach (var item in rewards.Select(reward => Tuple.Create(reward.Item, Commerce.MarketProfitAdjuster(Commerce.ValueSell(reward.Item.Key, false), reward.Item.Key, reward.Counts[0]) * reward.Counts[0])).OrderByDescending(tup => tup.Item2))
+        {
+            Dbg.Inf($"  {item.Item1.Name}: {item.Item2:F0}");
         }
     }
 }
