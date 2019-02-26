0cfef42c813a6d765d83eef65ceeabc6b4a76b2e
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2019-02-26 13:41:14 -0800

    Be careful about up-to-date market suggestions.

--- src/Bootstrap.cs
+++ src/Bootstrap.cs
@@ -98,14 +98,14 @@ public static class Bootstrap
         }
     }
 
-    public static Tuple<float, string> EvaluateItem(SaintCoinach.Xiv.Recipe recipe, SaintCoinach.Xiv.Item result, bool hq)
+    public static Tuple<float, string> EvaluateItem(SaintCoinach.Xiv.Recipe recipe, SaintCoinach.Xiv.Item result, bool hq, Market.Latency latency)
     {
-        float expectedRevenue = Commerce.ValueSell(result.Key, hq, Market.Latency.Standard);
-        string readable = $"{recipe.ClassJob.Name} {recipe.ResultItem.Name} {(hq ? "HQ" : "NQ")} ({recipe.ResultItem.Key}): expected revenue {Commerce.ValueSell(result.Key, hq, Market.Latency.Standard):F0}";
+        float expectedRevenue = Commerce.ValueSell(result.Key, hq, latency);
+        string readable = $"{recipe.ClassJob.Name} {recipe.ResultItem.Name} {(hq ? "HQ" : "NQ")} ({recipe.ResultItem.Key}): expected revenue {Commerce.ValueSell(result.Key, hq, latency):F0}";
         float tcost = 0;
         foreach (var ingredient in recipe.Ingredients)
         {
-            float cost = Commerce.ValueBuy(ingredient.Item.Key, false, Commerce.TransactionType.Immediate, Market.Latency.Standard, out string source);
+            float cost = Commerce.ValueBuy(ingredient.Item.Key, false, Commerce.TransactionType.Immediate, latency, out string source);
             readable += "\n" + $"  {ingredient.Item.Name}: buy from {source} for {cost:F0}x{ingredient.Count}";
 
             tcost += ingredient.Count * cost;
@@ -115,7 +115,7 @@ public static class Bootstrap
 
         // Adjust profit
         // HQing things is hard, assume we're willing to sell at most ten per day
-        float profitTimeAdjusted = profit * Math.Min(Commerce.MarketSalesPerDay(result.Key, Market.Latency.Standard), Math.Min(result.StackSize, hq ? 10 : 99));
+        float profitTimeAdjusted = profit * Math.Min(Commerce.MarketSalesPerDay(result.Key, latency), Math.Min(result.StackSize, hq ? 10 : 99));
         
         readable += "\n" + $"  Total cost: {tcost:F0}, total profit {profit:F0}, time-adjusted profit {profitTimeAdjusted:F0}";
         readable += "\n";
@@ -123,11 +123,15 @@ public static class Bootstrap
         return new Tuple<float, string>(profitTimeAdjusted, readable);
     }
 
+    private struct RecipeData
+    {
+        public Func<Market.Latency, Tuple<float, string>> evaluator;
+        public float estimate;
+    }
+
     public static void DoRecipeAnalysis(string classid, int levelmin, int hqcutoff, int levelmax, SortMethod sortMethod)
     {
-        var results = new List<Tuple<float, string>>();
-
-        var evaluator = new List<Action>();
+        var evaluators = new List<Func<Market.Latency, Tuple<float, string>>>();
 
         foreach (var recipe in Db.GetSheet<SaintCoinach.Xiv.Recipe>())
         {
@@ -156,30 +160,52 @@ public static class Bootstrap
 
             if (classLevel <= hqcutoff && result.CanBeHq)
             {
-                evaluator.Add(() => results.Add(EvaluateItem(recipe, result, true)));
+                evaluators.Add(latency => EvaluateItem(recipe, result, true, latency));
             }
 
-            evaluator.Add(() => results.Add(EvaluateItem(recipe, result, false)));
-        }
-
-        for (int i = 0; i < evaluator.Count; ++i)
-        {
-            Dbg.Inf($"{i} / {evaluator.Count}");
-            evaluator[i]();
+            evaluators.Add(latency => EvaluateItem(recipe, result, false, latency));
         }
 
         if (sortMethod == SortMethod.Order)
         {
-            foreach (var result in results)
+            foreach (var item in evaluators)
             {
-                Dbg.Inf(result.Item2);
+                Dbg.Inf(item(Market.Latency.Standard).Item2);
             }
         }
         else if (sortMethod == SortMethod.Profit)
         {
-            foreach (var result in results.OrderBy(result => result.Item1))
+            var recipeInfo = new List<RecipeData>();
+
+            for (int i = 0; i < evaluators.Count; ++i)
             {
-                Dbg.Inf(result.Item2);
+                Dbg.Inf($"{i} / {evaluators.Count}");
+
+                var result = evaluators[i](Market.Latency.Standard);
+                recipeInfo.Add(new RecipeData() {evaluator = evaluators[i], estimate = result.Item1});
+            }
+
+            recipeInfo = recipeInfo.OrderBy(x => x.estimate).ToList();
+
+            var goodRecipes = new List<RecipeData>();
+
+            while (recipeInfo.Count > 0 && (goodRecipes.Count < 20 || goodRecipes[0].estimate < recipeInfo[recipeInfo.Count - 1].estimate))
+            {
+                Dbg.Inf($"Immediate-testing; at {goodRecipes.Count} recipes");
+
+                var process = recipeInfo[recipeInfo.Count - 1];
+                recipeInfo.RemoveAt(recipeInfo.Count - 1);
+
+                process.estimate = process.evaluator(Market.Latency.Immediate).Item1;
+                goodRecipes.Add(process);
+
+                goodRecipes = goodRecipes.OrderBy(x => x.estimate).ToList();
+            }
+
+            foreach (var result in goodRecipes)
+            {
+                // Standard is safe here because we're already cached
+                Dbg.Inf(result.evaluator(Market.Latency.Standard).Item2);
             }
         }
         
